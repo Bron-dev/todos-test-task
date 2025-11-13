4481e8abb88ca91c560d832058dfda7d5dde91d0
Bron-dev <bronislav.webdev@gmail.com>
2025-11-13 22:10:21 +0200

    Remove redundant hook

--- src/components/HighlightedText/HighlightedText.tsx
+++ src/components/HighlightedText/HighlightedText.tsx
@@ -1,4 +1,4 @@
-import { useHighlightedText } from './useHighlightText.ts';
+import React from 'react';
 
 interface HighlightedTextProps {
   text: string;
@@ -6,7 +6,37 @@ interface HighlightedTextProps {
 }
 
 export const HighlightedText = ({ text, highlight }: HighlightedTextProps) => {
-  const parts = useHighlightedText(text, highlight);
+  if (!highlight) return <>{text}</>;
+
+  const lowerText = text.toLowerCase();
+  const lowerHighlight = highlight.toLowerCase();
+
+  const indices: number[] = [];
+  let lastIndex = -1;
+
+  for (const char of lowerHighlight) {
+    const index = lowerText.indexOf(char, lastIndex + 1);
+    if (index === -1) break;
+    indices.push(index);
+    lastIndex = index;
+  }
+
+  if (!indices.length) return <>{text}</>;
+
+  const parts: React.ReactNode[] = [];
+  let textIndex = 0;
+
+  for (const idx of indices) {
+    if (textIndex < idx) parts.push(text.slice(textIndex, idx)); // звичайний текст
+    parts.push(
+      <span key={idx} style={{ backgroundColor: 'yellow' }}>
+        {text[idx]}
+      </span>
+    );
+    textIndex = idx + 1;
+  }
+
+  if (textIndex < text.length) parts.push(text.slice(textIndex)); // решта тексту
 
   return <>{parts}</>;
 };
